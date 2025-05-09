9983e4d0498023f38760af2cda250db3fec9dc07
Antonn1812 <95938334+Antonn1812@users.noreply.github.com>
2025-05-09 11:56:53 +0200

    import leo in recommendation

--- recommendation.py
+++ recommendation.py
@@ -1,4 +1,5 @@
 import sqlite3
+import leo
 
 conn = sqlite3.connect('chansons.db')
 cursor = conn.cursor()
@@ -10,3 +11,6 @@ cursor.execute("SELECT * FROM chansons ORDER BY isrc ASC")
 result = cursor.fetchall()
 for lign in result:
     print(lign)
+
+first_song = leo.chanson_depart
+matrix = leo.matrice_transition
