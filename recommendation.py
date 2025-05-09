0914e6659a28f99fc6126c08c270194b0e5990af
Antonn1812 <95938334+Antonn1812@users.noreply.github.com>
2025-05-09 11:26:04 +0200

    find "chansons" with sql finish completely

--- recommendation.py
+++ recommendation.py
@@ -1,14 +1,12 @@
 import sqlite3
 
-# Connexion à la base de données (elle sera créée si elle n'existe pas)
 conn = sqlite3.connect('chansons.db')
 cursor = conn.cursor()
 
 
 
-# Insérer une donnée
-cursor.execute("SELECT * FROM chansons")
+cursor.execute("SELECT * FROM chansons ORDER BY isrc ASC")
 
-resultats = cursor.fetchall()
-for ligne in resultats:
-    print(ligne)
+result = cursor.fetchall()
+for lign in result:
+    print(lign)
